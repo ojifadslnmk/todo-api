fcdee2c7375bdb5cd7703ac344d2b9571dbfed4a
David <david.dados123@gmail.com>
2026-02-17 23:46:20 +0100

    setup SQLite configuration and database session management

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.db.base import Base
+from app.db.session import engine
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
